ab7d582490e40339bef6b6539f19f21a1a7c9d14
middleKD <kmiddlecenter@gmail.com>
2025-08-03 21:19:19 +0900

    update: a2a msa

---
+++ src/server/app_fasta2a.py
@@ -0,0 +1,27 @@
+from graph import convert_graph_as_agent
+from fasta2a.schema import AgentProvider, Skill
+
+agent = convert_graph_as_agent()
+
+provider = AgentProvider(
+    organization="seocho-data-study",
+    url="https://github.com/cdkkim/data-study"
+)
+agent_skill = Skill(
+    id="hallucination_check_agent",
+    name="Halucination Check Agent",
+    description="Halucination Check Agent",
+    tags=["halucination", "check"],
+    examples=["Check if the text is a hallucination"],
+    input_modes=["application/json"],
+    output_modes=["application/json"],
+)
+
+app = agent.to_a2a(
+    name="hallucination_check_agent",
+    url="http://localhost:8008",
+    version="0.0.1",
+    description="Halucination Check Agent",
+    provider=provider,
+    skills=[agent_skill],
+)
